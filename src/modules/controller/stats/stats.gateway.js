7033514ecd342b9542200beb6d484069b66e9cff
DiegoValentin1 <20213tn087@utez.edu.mx>
2023-12-19 16:37:20 -0600

    stats

--- src/modules/controller/stats/stats.gateway.js
+++ src/modules/controller/stats/stats.gateway.js
@@ -1,27 +1,27 @@
 const {query} = require('../../../utils/mysql');
 
 const findAllTotal = async()=>{
-    const sql = `SELECT fecha, SUM(total) as total FROM registro_alumnos group by fecha`;
+    const sql = `SELECT fecha, SUM(total) as total FROM larghetto.registro_alumnos WHERE YEAR(fecha) = YEAR(CURDATE()) group by fecha`;
     return await query(sql, []);
 }
 
 const findAllCentro = async()=>{
-    const sql = `SELECT campus, fecha, SUM(total) as total FROM registro_alumnos WHERE campus='centro' group by campus, fecha`;
+    const sql = `SELECT campus, fecha, SUM(total) as total FROM registro_alumnos WHERE campus='centro' AND YEAR(fecha) = YEAR(CURDATE()) group by campus, fecha`;
     return await query(sql, []);
 }
 
 const findAllBuga = async()=>{
-    const sql = `SELECT campus, fecha, SUM(total) as total FROM larghetto.registro_alumnos WHERE campus='bugambilias' group by campus, fecha`;
+    const sql = `SELECT campus, fecha, SUM(total) as total FROM larghetto.registro_alumnos WHERE campus='bugambilias' AND YEAR(fecha) = YEAR(CURDATE()) group by campus, fecha`;
     return await query(sql, []);
 }
 
 const findAllCuautla = async()=>{
-    const sql = `SELECT campus, fecha, SUM(total) as total FROM larghetto.registro_alumnos WHERE campus='cuautla' group by campus, fecha`;
+    const sql = `SELECT campus, fecha, SUM(total) as total FROM larghetto.registro_alumnos WHERE campus='cuautla' AND YEAR(fecha) = YEAR(CURDATE()) group by campus, fecha`;
     return await query(sql, []);
 }
 
 const findAllActual = async()=>{
-    const sql = `SELECT campus, count(id) as total FROM users WHERE role='ALUMNO' group by campus`;
+    const sql = `SSELECT us.campus, count(us.id) as total FROM users us JOIN alumno alu on alu.user_id=us.id WHERE us.role='ALUMNO' AND alu.estado != 0 group by us.campus`;
     return await query(sql, []);
 }
 
